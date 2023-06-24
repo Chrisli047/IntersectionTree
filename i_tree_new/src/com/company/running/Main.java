b006c61e1afa4cc01368387c7c9b14983f099de4
maximpopov11 <icecreamguy0@gmail.com>
2023-06-24 14:15:42 -0500

    Add todo for tech debt refactor plan
    * Tech Debt Refactor
    * Point Memorization Solutions (Backend and Local versions)
    * Data Gathering

--- i_tree_new/src/com/company/running/Main.java
+++ i_tree_new/src/com/company/running/Main.java
@@ -4,6 +4,24 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 // TODO: TECH DEBT REFACTOR
+//  * Solution Utilities
+//    * Function
+//    * Node Data
+//  * Parametric Equation Solution
+//    * Point
+//    * Segment
+//    * Partition
+//    * ParametricNodeData
+//  * Simplex Solution
+//    * SimplexMarker
+//    * SimplexType
+//    * TwoPhaseSimplex
+//    * TwoPhaseSignChangingSimplex
+//    * TwoPhaseSignChangingPointMemorizingSimplex
+//    * SimplexNodeData
+//  * Tree
+//  * Test
+//  * Main
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
         // TODO: migrate files into folders
